70528289a0d6b415cf1c7ab38e010057b95a2192
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-06-01 02:24:59 +0600

    Create warshall.cpp

---
+++ warshall.cpp
@@ -0,0 +1,60 @@
+/? warshall algorithm
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define INF 999999
+
+void printMatrix(const vector<vector<int>>& dist) {
+    int V = dist.size();
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            if (dist[i][j] == INF)
+                cout << "INF\t";
+            else
+                cout << dist[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void floydWarshall(vector<vector<int>>& graph) {
+    int V = graph.size();
+    vector<vector<int>> dist = graph;
+
+    for (int k = 0; k < V; ++k) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    cout << "Shortest distances between all pairs of vertices:" << endl;
+    printMatrix(dist);
+}
+
+int main() {
+    int V;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+
+    vector<vector<int>> graph(V, vector<int>(V));
+    cout << "Enter the weighted adjacency matrix (INF for no edge):" << endl;
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            cin >> graph[i][j];
+            if (i != j && graph[i][j] == 0) {
+                graph[i][j] = INF;
+            }
+        }
+    }
+
+    floydWarshall(graph);
+
+    return 0;
+}
