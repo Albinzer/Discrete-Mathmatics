0af8ca05e096f91e190cd720670270db46d72861
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-06-01 02:11:30 +0600

    Create relation r.cpp

---
+++ relation r.cpp
@@ -0,0 +1,52 @@
+// two set making relation r and matrices 
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int sA, sB;
+    set<int> A, B;
+
+    cout << "Enter size of A: ";
+    cin >> sA;
+    cout << "Enter size of B: ";
+    cin >> sB;
+
+    cout << "Enter elements of A:" << endl;
+    for (int i = 0; i < sA; i++) {
+        int eleA;
+        cin >> eleA;
+        A.insert(eleA);
+    }
+
+    cout << "Enter elements of B:" << endl;
+    for (int i = 0; i < sB; i++) {
+        int eleB;
+        cin >> eleB;
+        B.insert(eleB);
+    }
+
+    // Print Relation
+    cout << "Relation:" << endl;
+    for (int a : A) {
+        for (int b : B) {
+            if (a > b) {
+                cout << "(" << a << "," << b << "), ";
+            }
+        }
+    }
+    cout << endl;
+
+    // Print Matrix
+    cout << "Matrix:" << endl;
+    for (int a : A) {
+        for (int b : B) {
+            cout << (a > b ? 1 : 0) << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
