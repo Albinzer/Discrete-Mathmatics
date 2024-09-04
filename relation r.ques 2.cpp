b58bf3c93805f8dd0ac16a3687b75dea7b59248e
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-09-05 00:31:54 +0600

    Update relation r.ques 2.cpp

--- relation r.ques 2.cpp
+++ relation r.ques 2.cpp
@@ -6,14 +6,18 @@
 using namespace std;
 
 int main() {
-    int sA, sB;
-    set<int> A, B;
+    int sA, sB;  // Sizes of sets A and B
+    set<int> A, B;  // Sets A and B
 
+    // Input size of set A
     cout << "Enter size of A: ";
     cin >> sA;
+
+    // Input size of set B
     cout << "Enter size of B: ";
     cin >> sB;
 
+    // Input elements of set A
     cout << "Enter elements of A:" << endl;
     for (int i = 0; i < sA; i++) {
         int eleA;
@@ -21,6 +25,7 @@ int main() {
         A.insert(eleA);
     }
 
+    // Input elements of set B
     cout << "Enter elements of B:" << endl;
     for (int i = 0; i < sB; i++) {
         int eleB;
@@ -28,8 +33,8 @@ int main() {
         B.insert(eleB);
     }
 
-    // Print Relation
-    cout << "Relation:" << endl;
+    // Print the relation pairs where a > b
+    cout << "\nRelation R = {(a, b) | a > b}:" << endl;
     for (int a : A) {
         for (int b : B) {
             if (a > b) {
@@ -39,11 +44,11 @@ int main() {
     }
     cout << endl;
 
-    // Print Matrix
-    cout << "Matrix:" << endl;
+    // Print the matrix representation of the relation
+    cout << "\nMatrix Representation:" << endl;
     for (int a : A) {
         for (int b : B) {
-            cout << (a > b ? 1 : 0) << " ";
+            cout << (a > b ? 1 : 0) << " ";  // 1 if a > b, otherwise 0
         }
         cout << endl;
     }
